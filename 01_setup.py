dd4a482b333c527f0cdb65e1e4cef64df3083a59
Benjamin Chan <benjamin.v.chan@gmail.com>
2025-01-25 23:58:31 -0800

    01 script outline

---
+++ 01_setup.py
@@ -0,0 +1,6 @@
+# Import necessary modules
+# Define function to load configuration parameters from config.json
+# Define function to initialize population
+# - Input: Total population size, initial percentage of hawks and doves
+# - Output: List of agents (hawk/dove)
+# Save initialized population to a CSV file
