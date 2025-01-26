f6cea9cb668a3b564a04251669517341a19e7663
Benjamin Chan <benjamin.v.chan@gmail.com>
2025-01-26 00:09:27 -0800

    02 simulate script outline

---
+++ scripts/02_simulate.py
@@ -0,0 +1,8 @@
+# Import necessary modules
+# Define function to run a single round of interactions
+# - Input: Population, payoff matrix
+# - Output: Updated population and rewards
+# Define function to simulate multiple generations
+# - Input: Population, payoff matrix, number of generations
+# - Output: Time-series data of population ratios and rewards
+# Save simulation results to a CSV file
